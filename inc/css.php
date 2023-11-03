f5a8351732a2db2bf0aa364df1e3a712fff101b8
Binu_Tamang <137280783+Binu-Tamang@users.noreply.github.com>
2023-11-03 16:17:04 +0545

    first commit

---
+++ inc/css.php
@@ -0,0 +1,18 @@
+<!-- google fonts -->
+
+
+<!-- Fontawesome cdn -->
+<link media="all" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" type='text/css' media=all lazyload>
+
+<!-- bootstrap.js cdn-->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+   integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+<link media="all" rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+<!-- Aos css -->
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+<!-- FavIcon -->
+<link rel="icon" type="images/png" href="assets/img/">
+<!-- FavIcon -->
---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- font awesome,custom css,bootstrap css -->
+  <?php include('inc/css.php') ?>
+
+  <!-- favicon link -->
+  <link rel="icon" type="images/png" href="">
+  <title>Airdire City Taxi</title>
+</head>
+
+<body>
+  <header>
+    <div class="heading-1">
+      <div class="call-msg-header">
+        <div class="row">
+          <div class="col-md-6">
+            <ul class="contact-details">
+              <li><i class="fa-solid fa-phone"></i><a href="tel:403 351 2200">403 351 2200</a></li>
+              <li><i class="fa-solid fa-envelope"></i><a
+                  href="info@www.airdriecitytaxi.ca">info@www.airdriecitytaxi.ca</a>
+              </li>
+              <li><i class="fa-solid fa-location-dot"></i><a href="">1105 Saudi Arabia – Street, CA</a></li>
+            </ul>
+          </div>
+
+          <div class="col-md-6">
+            <ul class="social-media-i">
+              <li><a href="https://www.facebook.com/AirdrieCityTaxi"><i class="fa-brands fa-facebook"></i></i></a></li>
+              <li><a href="https://twitter.com/airdriecitytaxi"><i class="fa-brands fa-twitter"></i></a></li>
+              <li><a href="https://www.pinterest.co.uk/airdriecitytaxica/"><i class="fa-brands fa-pinterest"></i></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</body>
+
+</html>
